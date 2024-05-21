3cde4dd7837b86fd83d87c79c19f13134d91e5ee
Amir Kharazi <irantehranid@yahoo.com>
2024-05-21 17:47:30 +0330

    First Commit Event,ValueObject,Validation

---
+++ 01.Core/Bazzar.Core.Domain/Advertisements/Entitys/Advertisement.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using Bazzar.Core.Domain.Advertisements.ObjectValue;
+using Framework.Domain.Events;
+using Framework.Domain.Entitys;
+using Bazzar.Core.Domain.Advertisements.Events;
+using Framework.Tools.Enums;
+using Framework.Domain.Exceptions;
+
+namespace Bazzar.Core.Domain.Advertisements.Entitys
+{
+    public class Advertisment : BaseEntity<Guid>
+    {
+
+        #region Fields    
+        public UserId OwnerId { get; protected set; }
+        public UserId ApprovedBy { get; protected set; }
+        public AdvertismentTitle Title { get; protected set; }
+        public AdvertismentText Text { get; protected set; }
+        public Price Price { get; protected set; }
+        public AdvertismentState State { get; protected set; }
+        #endregion
+
+        private Advertisment()
+        {
+
+        }
+
+        #region Event
+        public Advertisment(Guid id, UserId ownerId) =>
+            HandleEvent(new AdvertismentCreated
+            {
+                Id = id,
+                OwnerId = ownerId.Value
+            });
+
+
+        public void SetTitle(AdvertismentTitle title) =>
+            HandleEvent(new AdvertismentTitleChanged
+            {
+                Id = Id,
+                Title = title.Value
+            });
+        public void UpdateText(AdvertismentText text) =>
+            HandleEvent(new AdvertismentTextUpdated
+            {
+                Id = Id,
+                AdvertismentText = text.Value
+            });
+
+        public void UpdatePrice(Price price) =>
+            HandleEvent(new AdvertismentPriceUpdated
+            {
+                Id = Id,
+                Price = price.Value.Value
+            });
+
+        public void RequestToPublish() =>
+            HandleEvent(new AdvertismentSentForReview
+            {
+                Id = Id
+            });
+
+        #endregion
+
+        protected override void ValidateInvariants()
+        {
+            var isValid =
+                Id != null &&
+                OwnerId != null &&
+                (State switch
+                {
+                    AdvertismentState.ReviewPending =>
+                        Title != null
+                        && Text != null
+                        && Price != null,
+                    //&& !Pictures.Any(),
+                    AdvertismentState.Active =>
+                        Title != null
+                        && Text != null
+                        && Price != null
+                        && ApprovedBy != null,
+                    //&& !Pictures.Any(),
+                    _ => true
+                });
+            if (!isValid)
+            {
+                throw new InvalidEntityStateException(this, $"مقدار تنظیم شده برای آگهی در وضیعت {State.GetDescription()} غیر قابل قبول است");
+            }
+        }
+
+        protected override void SetStateByEvent(IEvent @event)
+        {
+            switch (@event)
+            {
+                case AdvertismentCreated e:
+                    Id = e.Id;
+                    OwnerId = new UserId(e.OwnerId);
+                    State = AdvertismentState.Inactive;
+                    break;
+                case AdvertismentPriceUpdated e:
+                    Price = new Price(Rial.FromLong(e.Price));
+                    break;
+                case AdvertismentSentForReview e:
+                    State = AdvertismentState.ReviewPending;
+                    break;
+                case AdvertismentTextUpdated e:
+                    Text = new AdvertismentText(e.AdvertismentText);
+                    break;
+                case AdvertismentTitleChanged e:
+                    Title = new AdvertismentTitle(e.Title);
+                    break;
+
+                default:
+                    throw new InvalidOperationException("امکان اجرای عملیات درخواستی وجود ندارد");
+            }
+        }
+    }
+}
---
+++ 01.Core/Bazzar.Core.Domain/Advertisements/ObjectValue/AdvertismentText.cs
@@ -0,0 +1,32 @@
+﻿using Framework.Domain.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bazzar.Core.Domain.Advertisements.ObjectValue
+{
+    public class AdvertismentText : BaseValueObject<AdvertismentText>
+    {
+
+        public string Value { get; private set; }
+        public static AdvertismentText FromString(string value) => new AdvertismentText(value);
+        private AdvertismentText()
+        {
+
+        }
+        public AdvertismentText(string value)
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                throw new ArgumentException("برای متن آگهی مقدار لازم است", nameof(value));
+            }
+            Value = value;
+        }
+        public override int ObjectGetHashCode() => Value.GetHashCode();
+        public override bool ObjectIsEqual(AdvertismentText otherObject) => Value == otherObject.Value;
+
+        public static implicit operator string(AdvertismentText advertismentText) => advertismentText.Value;
+    }
+}
